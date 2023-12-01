34d9dffc5c5de123e521b118456dcedd94a18f2a
DipsikKk <141740406+DearDany@users.noreply.github.com>
2023-12-01 13:21:51 +0200

    done tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,40 +1,18 @@
-/*Условия
-1. Функція makeTransaction(quantity, pricePerDroid, customerCredits), яка складає та повертає повідомлення про купівлю ремонтних дроїдів.
+/*
+1. Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
 
-2.
-quantity — кількість замовлених дроїдів
-pricePerDroid — ціна одного дроїда
-customerCredits — сума коштів на рахунку клієнта
-
-3. Оголоси змінну для зберігання загальної суми замовлення (загальна вартість усіх замовлених дроїдів) і задай їй вираз розрахунку цієї суми.
-
-4.Додай перевірку, чи зможе клієнт оплатити замовлення:
-якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, функція має повертати рядок "Insufficient funds!"
-в іншому випадку функція має повертати рядок "You ordered <quantity> droids worth <totalPrice> credits!", де <quantity> 
-це кількість замовлених дроїдів, а <totalPrice> це їх загальна вартість.
-
-
+2.Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+Усі символи slug повинні бути в нижньому регістрі.
+Усі слова slug повинні бути розділені тире.
 */
 
-// let quantity - кол-во дройдов
-// const pricePerDroid  - цена за дройд
-// let customerCredits  - сумма денег на счету клиента
 
-
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let totalPrice = quantity * pricePerDroid;
-
-    if(totalPrice <= customerCredits){
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    } else {
-        return `Insufficient funds!`
-    }
-   
+function slugify(title) {
+    const slug = title.toLowerCase().split(" ").join("-");
+    return slug
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-3.js
+++ js/task-3.js
@@ -1,30 +1,25 @@
 /*
-1.Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale,
- і повертає результат перевірки.
+filterArray(numbers, value), яка приймає масив чисел (numbers) та значення (value) як параметри.
+ Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value.
 
-2.
-- Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
--Якщо в рядку відсутні заборонені слова, функція повертає буль false
-
-step 1: привести все сообщение к одному регистру
-step2: проверить на наличие слов из ЧС
-step3: вернуть результат
-
+ 2.Створи порожній масив, у який будеш додавати підходящі числа.
+-Використай цикл для ітерації кожного елемента масиву numbers.
+-Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
+-Поверни свій новий масив з підходящими числами як результат.
 */
 
-function checkForSpam(message) {
-    let checkSpam = message.toLowerCase();
-    if (checkSpam.includes('spam') || checkSpam.includes ('sale')) {
-        return true;
-    } else {
-        return false;
+function filterArray(numbers, value) {
+    let filteredNumbers = [];
+    for (const number of numbers) {
+        if (number > value) {
+            filteredNumbers.push(number);
+        }
     }
+    return filteredNumbers;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
